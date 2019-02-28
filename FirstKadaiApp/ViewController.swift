53007790a5705308622f30536831bbdee6b55a90
USER <user@user-no-macbook-pro.local>
2019-02-28 22:26:45 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by USER on 2019/02/28.
+//  Copyright © 2019年 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FirstKadaiAppTests/FirstKadaiAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  FirstKadaiAppTests.swift
+//  FirstKadaiAppTests
+//
+//  Created by USER on 2019/02/28.
+//  Copyright © 2019年 USER. All rights reserved.
+//
+
+import XCTest
+@testable import FirstKadaiApp
+
+class FirstKadaiAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
